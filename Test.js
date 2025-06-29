f955ac4cf82734e03677ea3ca462114db99df3d7
Adil Quraishi <adilquraishi2021@gmail.com>
2025-06-29 17:56:03 +0530

    Initial commit

--- Test.js
+++ Test.js
@@ -1,1 +1,19 @@
 console.log("Adil")
+console.log("Adil2")
+
+const id = 1;
+let name = "Adil";
+var age = 21;
+city = "Noida";
+// id = 2;
+
+name = "Saransh";
+age = 22;
+city = "Lucknow";
+console.log(id);
+
+console.log(name);
+console.log(age);       
+console.log(city);
+
+console.table({ id, name, age, city });
