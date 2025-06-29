723a7e5744c349e68614ef75f1d98c5e0cfbd8b8
Adil Quraishi <adilquraishi2021@gmail.com>
2025-06-29 17:47:52 +0530

    Datatypes

---
+++ 01Datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all code as newer version 
+
+// number
+// bigint
+// string       
+// boolean
+// symbol => unique identifier
+// undefined
+// null => intentional absence of any value
+
+// object => collection of key-value pairs
+console.log("Adil");
+console.log(typeof undefined); // undefined is a type in JS, it means the variable has been declared but not assigned a value
+
+console.log(typeof null); // null is an object in JS, this is a bug in JS
